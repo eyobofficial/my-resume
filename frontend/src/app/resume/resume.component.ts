26ad1eed28b78403529c924f87fb02b9ba534d2e
Eyob Tariku <eyobtariku@gmail.com>
2020-08-08 17:39:48 +0300

    Revert back to bootstrap 4 & fix carousel resize bug

--- frontend/src/app/resume/resume.component.ts
+++ frontend/src/app/resume/resume.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { environment } from '@environments/environment';
 
 @Component({
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit {
 
+  private pageTitle = `${environment.pageTitle} - Resume`;
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.pageTitle);
+  }
 }
