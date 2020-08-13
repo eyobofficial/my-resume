29cc5d7de3e92a3f4efcdbbb062d7cc63c1b7d2c
Eyob Tariku <eyobtariku@gmail.com>
2020-08-13 19:30:02 +0300

    Remove unused assets & fix mobile nav toggle

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  mobileNav = false;
 
+  toggleMobileNav(): void {
+    this.mobileNav = !this.mobileNav;
+  }
 }
